919e3b1ba8ec6034837c9e88b501f595c2bd6720
Mateusz <mateuszdybala2@gmail.com>
2022-11-20 18:50:57 +0100

    added + operator & fixed <<

--- polynomial/Poly.cpp
+++ polynomial/Poly.cpp
@@ -5,23 +5,45 @@ Poly::Poly()
 
 }
 
+Poly::Poly(double d)
+{
+    this->coef[0] = d;
+}
+
 Poly::Poly(const Poly& p)
 {
-    this->coefficients = p.coefficients;
+    this->coef = p.coef;
 }
 
 double& Poly::operator[](size_t pos)
 {
-    return this->coefficients[pos];
+    return this->coef[pos];
+}
 
+Poly Poly::operator+(const Poly& p) const
+{
+    Poly result = Poly(*this);
+
+    for (auto pair = p.coef.begin(); pair != p.coef.end(); pair++) {
+        int key = pair->first;
+        double val = pair->second;
+
+        if (result.coef.count(key)) {
+            result.coef[key] += val;
+        }else {
+            result.coef[key] = val;
+        }
+    }
+
+    return result;
 }
 
 std::ostream& operator<<(ostream& s, const Poly& p)
 {
-    // TODO reverse order
-    for (auto const& x : p.coefficients)
-    {
-        s << x.second << "x^" << x.first << "  ";
+    for (auto pair = p.coef.crbegin(); pair != p.coef.crend(); pair++) {
+        char buffer[50];
+        sprintf(buffer, "%+.8g", pair->second);
+        s << buffer << "x^" << pair->first << " ";
     }
     s << endl;
     return s;
--- polynomial/Poly.h
+++ polynomial/Poly.h
@@ -9,11 +9,13 @@ using namespace std;
 class Poly {
   public:
     Poly();
+    Poly(double d);
     Poly(const Poly& p);
     double& operator[](size_t pos);
+    Poly operator+(const Poly& p) const;
 //    double operator[](size_t pos) const;
   private:
-    map<int, double> coefficients;
+    map<int, double> coef;
     friend std::ostream& operator<<(std::ostream& s, const Poly& p);
 };
 
